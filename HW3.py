ea705386eb617152788c019710792a2a7eeed3a3
bbretana1178 <82167969+bbretana1178@users.noreply.github.com>
2021-04-16 14:20:20 -0500

    Week 12 Assignment 2_BriannaBretana

--- HW3.py
+++ HW3.py
@@ -1,20 +1,30 @@
 #Brianna Bretana
-#Program creates a .txt file adds data and appends data in file
+#Program opens previously created .txt file and reads the full thing in one function and only 3 lines in 
+#the second function
 #Week 12 Assignment 2
 
+#Created the first function
 def text_one():
+#Opens the cartoon file to read mode
     infile=open('cartoon.txt','r')
+#Creates variable for the read commpand
     file_content=infile.read()
+#Close file and print the variable/file contents
     infile.close()
     print(file_content)
 
+#Second function
 def text_two():
+#Opens with read mode
     infile=open('cartoon.txt','r')
+#Reads the first three lines from the file and creates variables for the lines
     line1=infile.readline()
     line2=infile.readline()
     line3=infile.readline()
+#Close file
     infile.close()
 
+#Prints the lines supressing the newline commands
     print(line1,end='')
     print(line2,end='')
     print(line3,end='')
